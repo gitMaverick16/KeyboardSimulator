af7c4c204c8c6bbc7f833621cfdcccaf0ad64759
Sergio Rondon <sergio19980816@gmail.com>
2024-10-27 22:50:58 -0500

    Agregar archivos de proyecto.

---
+++ KeyboardSimulator/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace KeyboardSimulator
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tbHello.Text = "Hello";
+
+
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
