0fb08a9fcbb58faae5c23ca08c4e1955d15aea23
Sergio Rondon <sergio19980816@gmail.com>
2024-10-30 23:14:26 -0500

    add configuration for mediaplayer

--- KeyboardSimulator/MainWindow.xaml.cs
+++ KeyboardSimulator/MainWindow.xaml.cs
@@ -1,20 +1,77 @@
-﻿using System.Windows;
+﻿using System.Media;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
 
 namespace KeyboardSimulator
 {
 
     public partial class MainWindow : Window
     {
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+
         public MainWindow()
         {
             InitializeComponent();
-
-
+            Loaded += MainWindow_Loaded;
+            Closed += MainWindow_Closed;
         }
 
-        private void btnRun_Click(object sender, RoutedEventArgs e)
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            _hookID = SetHook(_proc);
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (var curProcess = System.Diagnostics.Process.GetCurrentProcess())
+            using (var curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                // Crear una nueva instancia de MediaPlayer en cada pulsación de tecla
+                MediaPlayer mediaPlayer = new MediaPlayer();
+                mediaPlayer.Open(new Uri(@"C:/Users/usuario/Documents/Projects/KeyboardSimulator/KeyboardSimulator/Assets/Audio/Alpaca/ENTER.mp3"));
+                    mediaPlayer.Volume = 1; // Ajuste de volumen
+                mediaPlayer.Play();
+
+                // Liberar el recurso cuando termine de reproducir el sonido
+                mediaPlayer.MediaEnded += (s, e) => mediaPlayer.Close();
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
     }
 }
