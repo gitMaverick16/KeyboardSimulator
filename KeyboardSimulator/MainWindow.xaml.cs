2aaa0ee3cffa0d53b35aa15b761d60a7100c0d21
Sergio Rondon <sergio19980816@gmail.com>
2024-11-02 18:16:29 -0500

    add logic to handle sound style and volume

--- KeyboardSimulator/MainWindow.xaml.cs
+++ KeyboardSimulator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Media;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace KeyboardSimulator
@@ -27,10 +28,25 @@ namespace KeyboardSimulator
 
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
 
         private static LowLevelKeyboardProc _proc = HookCallback;
         private static IntPtr _hookID = IntPtr.Zero;
 
+        private static string StyleSound = "Alpaca";
+        private static double Volume = 50;
+        private static bool IsMuted = false;
+
+        private static string EnterPressSoundPath = "/Assets/Audio/Style/Press/ENTER.mp3";
+        private static string EnterReleaseSoundPath = "/Assets/Audio/Style/Up/ENTER.mp3";
+        private static string SpacePressSoundPath = "/Assets/Audio/Style/Press/SPACE.mp3";
+        private static string SpaceReleaseSoundPath = "/Assets/Audio/Style/Up/SPACE.mp3";
+        private static string BackspacePressSoundPath = "/Assets/Audio/Style/Press/BACKSPACE.mp3";
+        private static string BackspaceReleaseSoundPath = "/Assets/Audio/Style/Up/BACKSPACE.mp3";
+        private static string DefaultPressSoundPath = "/Assets/Audio/Style/Press/GENERIC.mp3";
+        private static string DefaultReleaseSoundPath = "/Assets/Audio/Style/Up/GENERIC.mp3";
+
+        private static HashSet<int> _pressedKeys = new HashSet<int>();
 
         public MainWindow()
         {
@@ -60,19 +76,77 @@ namespace KeyboardSimulator
 
         private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            var test = EnterPressSoundPath.Replace("Style", StyleSound);
+            if (nCode >= 0 && !IsMuted)
             {
-                // Crear una nueva instancia de MediaPlayer en cada pulsación de tecla
-                MediaPlayer mediaPlayer = new MediaPlayer();
-                mediaPlayer.Open(new Uri(Directory.GetCurrentDirectory() + @"/Assets/Audio/press/Alpaca/ENTER.mp3"));
-                mediaPlayer.Volume = 1; // Ajuste de volumen
-                mediaPlayer.Play();
+                int vkCode = Marshal.ReadInt32(lParam); // virtual key code
+                string soundPath = string.Empty;
 
-                // Liberar el recurso cuando termine de reproducir el sonido
-                mediaPlayer.MediaEnded += (s, e) => mediaPlayer.Close();
+                if (wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    if (!_pressedKeys.Contains(vkCode))
+                    {
+                        _pressedKeys.Add(vkCode);
+                        soundPath = vkCode switch
+                        {
+                            0x0D => EnterPressSoundPath.Replace("Style", StyleSound),      // Enter
+                            0x20 => SpacePressSoundPath.Replace("Style", StyleSound),      // Space
+                            0x08 => BackspacePressSoundPath.Replace("Style", StyleSound),  // Backspace
+                            _ => DefaultPressSoundPath.Replace("Style", StyleSound)        // Todas las demás teclas
+                        };
+                    }
+                }
+                else if (wParam == (IntPtr)WM_KEYUP)
+                {
+                    if (_pressedKeys.Contains(vkCode))
+                    {
+                        _pressedKeys.Remove(vkCode);
+                        soundPath = vkCode switch
+                        {
+                            0x0D => EnterReleaseSoundPath.Replace("Style", StyleSound),     // Enter
+                            0x20 => SpaceReleaseSoundPath.Replace("Style", StyleSound),     // Space
+                            0x08 => BackspaceReleaseSoundPath.Replace("Style", StyleSound), // Backspace
+                            _ => DefaultReleaseSoundPath.Replace("Style", StyleSound)       // Todas las demás teclas
+                        };
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(soundPath))
+                {
+                    PlaySound(soundPath);
+                }
             }
 
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static void PlaySound(string path)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(Directory.GetCurrentDirectory() + path));
+            mediaPlayer.Volume = Volume / 100;
+            mediaPlayer.Play();
+            mediaPlayer.MediaEnded += (s, e) => mediaPlayer.Close();
+        }
+
+        private void MuteCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            IsMuted = true;
+        }
+
+        private void MuteCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            IsMuted = false;
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Volume = (int)VolumeSlider.Value;
+        }
+
+        private void SoundComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            StyleSound = (SoundComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
         }
     }
 }
